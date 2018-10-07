da22557d4c628a4120765d30e7f3b531417ba204
Jose Carmona <j.carmona.n@gmail.com>
2018-10-07 23:41:40 +0200

    borra container después de ejecutar

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name apache -p 8080:8080 -p 8443:8443 -it -v `pwd`:/app bitnami/apache:latest
+docker run --name apache -p 8080:8080 -p 8443:8443 -it --rm -v `pwd`:/app bitnami/apache:latest
