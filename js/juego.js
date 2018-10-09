4f9434d118352b9633b58dbdcb2c961cbae78469
Jose Carmona <j.carmona.n@gmail.com>
2018-10-09 20:37:37 +0200

    camara se mueve siguiendo al actor y es posible mover el actor con teclas de cursor

--- js/juego.js
+++ js/juego.js
@@ -10,9 +10,13 @@ var config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
+
+var cursors;
+var actor;
 
 var game = new Phaser.Game(config);
 
@@ -27,7 +31,13 @@ function preload ()
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  this.cameras.main.setBounds(0, 0, 800*2, 600*2);
+  this.physics.world.setBounds(0, 0, 800*2, 600*2);
+  this.add.tileSprite(0, 0, 3400*2, 1000*2, 'sky');
+
 
     var particles = this.add.particles('red');
 
@@ -37,11 +47,35 @@ function create ()
         blendMode: 'ADD'
     });
 
-    var actor = this.physics.add.image(400, 100, 'actor');
+    actor = this.physics.add.image(400, 100, 'actor');
 
     actor.setVelocity(100, 200);
     actor.setBounce(1, 1);
     actor.setCollideWorldBounds(true);
 
+    this.cameras.main.startFollow(actor);
+
     emitter.startFollow(actor);
+}
+
+function update ()
+{
+
+    if (cursors.left.isDown)
+    {
+        actor.setVelocityX(-100);
+    }
+    else if (cursors.right.isDown)
+    {
+        actor.setVelocityX(100);
+    }
+
+    if (cursors.up.isDown)
+    {
+        actor.setVelocityY(-300);
+    }
+    else if (cursors.down.isDown)
+    {
+        actor.setVelocityY(300);
+    }
 }
